25a9b3927d381442890551275ae1039f61c74c5a
Auriarte20 <uriarte2001alexis@gmail.com>
2022-12-14 10:40:52 -0500

    Refactor: add trycatch by routes

--- src/routes/players.routes.ts
+++ src/routes/players.routes.ts
@@ -1,35 +1,55 @@
 import { Router } from "express";
 import * as playerServices from "../services/service";
+import toNewPlayer from "../utils/utils";
 
 const router = Router();
 
 router.get("/", (_req, res) => {
-  const allPlayers = playerServices.getPlayers();
-  res.send(allPlayers);
+  try {
+    const allPlayers = playerServices.getPlayers();
+    res.status(200).json({
+      msg: "Todos los jugadores",
+      allPlayers,
+    });
+  } catch (error: any) {
+    res.status(500).send({ msg: error.message });
+  }
 });
 
 router.get("/notTeam", (_req, res) => {
-  const playersNotTeams = playerServices.getPlayersWithoutTeam();
-  res.status(200).json({
-    msg: "Información sin su equipo deportivo",
-    playersNotTeams,
-  });
+  try {
+    const playersNotTeams = playerServices.getPlayersWithoutTeam();
+    res.status(200).json({
+      msg: "Información sin su equipo deportivo",
+      playersNotTeams,
+    });
+  } catch (error: any) {
+    res.status(500).send({ msg: error.message });
+  }
 });
 
 router.get("/:id", (req, res) => {
-  const player = playerServices.getPlayerId(Number(req.params.id));
-  return player
-    ? res.status(200).json({ msg: "Jugador encontrado", player })
-    : res.status(404).send({ msg: "Jugador inexistente" });
+  try {
+    const player = playerServices.getPlayerId(Number(req.params.id));
+    return player
+      ? res.status(200).json({ msg: "Jugador encontrado", player })
+      : res.status(404).send({ msg: "Jugador inexistente" });
+  } catch (error: any) {
+    res.status(400).send({ msg: error.message });
+  }
 });
 
 router.post("/", (req, res) => {
-  const newPlayer = toNewPlayer(req.body);
-  const createdPlayer = playerServices.addPlayer(newPlayer);
-  res.status(201).json({
-    msg: `¡El jugador ${newPlayer.dorso} se ha subido con éxito!`,
-    createdPlayer,
-  });
+  try {
+    const newPlayer = toNewPlayer(req.body);
+    const createdPlayer = playerServices.addPlayer(newPlayer);
+    res.status(201).json({
+      msg: `¡El jugador ${newPlayer.dorso} se ha subido con éxito!`,
+      createdPlayer,
+    });
+  } catch (error: any) {
+    res.status(400).send({ msg: error.message });
+  }
 });
 
 export default router;
